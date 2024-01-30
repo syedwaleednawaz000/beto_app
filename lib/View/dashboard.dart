d5d4ad22f8dfa45400a81f9636e73f0e2aca2232
syedwaleednawaz000 <syedwaleednawaz333@gmail.com>
2024-01-31 00:03:32 +0500

    by waleed

--- lib/View/dashboard.dart
+++ lib/View/dashboard.dart
@@ -10,20 +10,25 @@ class DashBoard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Color(0xff009E90),
+          onPressed: (){
+            // Get.to(()=> ObservationScreen());
+          },child: Icon(Icons.add,color: Colors.white,)),
       body: Container(
         height: MediaQuery.of(context).size.height,
         child: Stack(
           children: [
             CustomAppBar(onTap: () {  },title: 'Pasos a realizar'),
             Container(
-              padding: EdgeInsets.only(top: 100,right: 15.w,left: 15.w),
+              margin: EdgeInsets.only(top: 100,right: 15.w,left: 15.w),
+
               child: ListView.builder(
                 itemCount: 3,
                 itemBuilder: (context, index) {
                 return Container(
-                  height: 168.h,
+                  // height: 168.h,
                 decoration:  BoxDecoration(
-                  // color: AppColor.whiteColor,
                   borderRadius: BorderRadius.circular(16),
                   image: DecorationImage(
                     image: AssetImage(index == 0 ? AppImages.backBlue : AppImages.backWhite),fit: BoxFit.cover)
@@ -31,24 +36,70 @@ class DashBoard extends StatelessWidget {
                 child: Column(
                   children: [
                     SizedBox(height: 20,),
-                    Container(
-                      height: 50,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding:  EdgeInsets.only(left: 15.w),
+                          child: Text(
                               "Finanzas Personales",
                               style: TextStyle(
-                                fontSize: 20,
+                                fontSize: 18,
                                 color: AppColor.purpleColor,
                                 fontWeight: FontWeight.w500,
                               )
                           ),
-                          SizedBox(width: 40.w,),
-                          Text("first "),
-                        ],
-                      ),
+                        ),
+                        Spacer(),
+                    SizedBox(
+                      width: 99,
+                      child: Text(
+                        "Puntos a obtener",
+                        style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      )
+                                      ),
                     ),
+                      ],
+                    ),
+                    SizedBox(height: 20.h,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          width: 170,
+                          child: Text(
+                              "Puedes destinar un pequeño % de tu flujo de efectivo (5% - 10%) a FMTY para ir acumulando ingresos pasivos en un instrumento de calidad, hazlo a través de GBM+.",
+                              style: TextStyle(
+                                fontSize: 10,
+                                fontWeight: FontWeight.w400,
+                              )
+                          ),
+                        ),
+                        // Spacer(),
+                        Text(
+                            "20 pts",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                            )
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 15.h,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                            index == 0 ? "":"Ver tutorial",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            )
+                        ),
+                      ],
+                    )
                   ],
                 ),
                 );
