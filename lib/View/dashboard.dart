83546bb432b8fef2eac78efcdb9e25d6f6508f01
syedwaleednawaz000 <syedwaleednawaz333@gmail.com>
2024-01-31 00:06:20 +0500

    by waleed

--- lib/View/dashboard.dart
+++ lib/View/dashboard.dart
@@ -1,8 +1,10 @@
 import 'package:betoapp/Utils/app_color.dart';
 import 'package:betoapp/Utils/app_images.dart';
+import 'package:betoapp/View/listado_history.dart';
 import 'package:betoapp/Widget/app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/route_manager.dart';
 
 class DashBoard extends StatelessWidget {
   const DashBoard({super.key});
@@ -13,7 +15,7 @@ class DashBoard extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
           backgroundColor: Color(0xff009E90),
           onPressed: (){
-            // Get.to(()=> ObservationScreen());
+            Get.to(()=> ListadoHistory());
           },child: Icon(Icons.add,color: Colors.white,)),
       body: Container(
         height: MediaQuery.of(context).size.height,
---
+++ lib/View/listado_history.dart
@@ -0,0 +1,28 @@
+import 'package:betoapp/Utils/app_color.dart';
+import 'package:betoapp/Utils/app_images.dart';
+import 'package:betoapp/Widget/app_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ListadoHistory extends StatelessWidget {
+  const ListadoHistory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Color(0xff009E90),
+          onPressed: (){
+            // Get.to(()=> ObservationScreen());
+          },child: Icon(Icons.add,color: Colors.white,)),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            CustomAppBar(onTap: () {  },title: 'Listado de gastos'),
+          ],
+        ),
+      ),
+    );
+  }
+}
